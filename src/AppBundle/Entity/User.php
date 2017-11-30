53f66f621a7f772c46b40e90d9b569a01316c1b7
roman-curse <frspm.roman@gmail.com>
2017-11-30 14:21:31 +0200

    user added

---
+++ src/AppBundle/Entity/User.php
@@ -0,0 +1,34 @@
+<?php
+
+/*
+ * Created by Roman Senchuk.
+ * as the part of the test Task for MoneyFGE
+ * at 30.11.17 14:13
+ */
+
+namespace AppBundle\Entity;
+
+use FOS\UserBundle\Model\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Class User
+ * @package AppBundle\Entity
+ * @author Roman Senchuk frspm.roman@gmail.com
+ * @ORM\Entity
+ * @ORM\Table(name="users")
+ */
+class User extends BaseUser
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+}
